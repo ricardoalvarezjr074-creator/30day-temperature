2562af5d08e7fbb8a7c18ddcb888ac27b8294ce5
rick a <ricksa925@gmail.com>
2026-09-18 21:31:45 -0700

    update

--- main.cpp
+++ main.cpp
@@ -3,3 +3,29 @@
 #include <array>
 #include <vector>
 using namespace std;  
+
+int main()
+{
+    const int SIZE = 30;
+    array <double, SIZE> temperatures;
+    ifstream fin;
+    fin.open("temperatures.txt");
+
+    if (fin.good())
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            fin >> temperatures.at(i);
+        }
+        fin.close();
+    }
+    else
+    {
+        cout << "File not found." << endl;
+        return 1;
+    }
+    
+    cout << "STD::array of temperatures:" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Number of temperatures: " << temperatures.size() << endl;
+}
